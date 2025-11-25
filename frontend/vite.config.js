600a4545d2cc6d0e74738380b7bf67810e546b4d
prasanna <prasannatwenty@gmail.com>
2025-11-25 18:59:12 +0530

    allows sub-domain

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['fk4wkwgkog04kk4c0gc48sc4.95.216.212.202.sslip.io']
+  }
 })
